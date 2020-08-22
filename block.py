596572703182bea66f0bc0ac456caaeac8071b5c
Robert Lima <ge.crio123@gmail.com>
2020-08-22 10:11:05 -0300

    Update block.py

--- block.py
+++ block.py
@@ -1,16 +1,27 @@
 import hashlib
 import time
+import binascii
+
 class Block:
-    __init__(self, index, previousHash, timestamp, data, hash):
+    __init__(self, index, previousHash, timestamp, data, hash, difficulty, nonce):
     self.index = index
     self.previousHash = previousHash
     self.timestamp = timestamp
     self.data = data
     self.hash = hash
+    self.difficulty = difficulty
+    self.nonce = nonce
 
 def calculateHash(index, previousHash, timestamp, data):
-    return hashlib.sha256(str(index) + previousHash + str(timestamp) + data).hexdigest()
+    return hashlib.sha256(str(index) + previousHash + str(timestamp) + data).enconde('UTF-8').hexdigest()
 
 ts = int(round(time.time()*1000)) 
 
 genesisBlock = Block(0, "", ts, "Genesis Block", calculateHash(0, "", ts, "Genesis Block"))
+
+def hashMatchesDifficulty(self,hash, difficulty):
+    hashBinary = binascii.unhexlify(hash)
+    requiredPrefix = '0'*int(difficulty)
+    return hashBinary.startswith(requiredPrefix)
+
+def findBlock
